b37e779f02f46b3f161759bd7c454d23eb1b20fe
misiekhardcore <misiekhardcore@gmail.com>
2021-04-08 21:39:46 +0200

    create empty components

---
+++ frontend/src/pages/CartPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+interface CartPageProps {}
+
+export const CartPage: React.FC<CartPageProps> = () => {
+  return <div className="cart">Cart</div>;
+};
---
+++ frontend/src/pages/ProductPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+interface ProductPageProps {}
+
+export const ProductPage: React.FC<ProductPageProps> = () => {
+  return <div className="product">Product</div>;
+};
