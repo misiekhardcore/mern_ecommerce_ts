5dd6b4157e9c9a63ac65a58702074afd7493420c
misiekhardcore <misiekhardcore@gmail.com>
2021-04-08 21:37:45 +0200

    move components folder to src

---
+++ frontend/src/components/Backdrop.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+interface BackdropProps {}
+
+export const Backdrop: React.FC<BackdropProps> = () => {
+  return <div className=""></div>;
+};
---
+++ frontend/src/components/SideDrawer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+interface SideDrawerProps {}
+
+export const SideDrawer: React.FC<SideDrawerProps> = () => {
+  return <div className=""></div>;
+};
