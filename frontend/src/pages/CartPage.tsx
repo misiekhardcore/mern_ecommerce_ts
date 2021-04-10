01abf0718b942dd75e8107c0ff8fb0c205556e11
misiekhardcore <misiekhardcore@gmail.com>
2021-04-10 22:42:44 +0200

    start making cart page

--- frontend/src/pages/CartPage.tsx
+++ frontend/src/pages/CartPage.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { CartItem } from "../components/CartItem";
 
 interface CartPageProps {}
 
 export const CartPage: React.FC<CartPageProps> = () => {
-  return <div className="cart">Cart</div>;
+  return (
+    <div className="cart">
+      <div className="cardpage__left">
+        <h2>Shopping Cart</h2>
+        <CartItem />
+      </div>
+      <div className="cardpage__right">
+        <div className="cartpage__info">
+          <p>Subtotal (0) items</p>
+          <p>$499.99</p>
+        </div>
+        <div className="button">
+          <button type="button">Checkout</button>
+        </div>
+      </div>
+    </div>
+  );
 };
