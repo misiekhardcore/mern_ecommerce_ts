28360fafa7d6e99eb3821d6978ee83cfcb91b045
misiekhardcore <misiekhardcore@gmail.com>
2021-04-10 22:41:47 +0200

    empty cartitem component

---
+++ frontend/src/components/CartItem.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./CartItem.scss";
+
+interface CartItemProps {}
+
+export const CartItem: React.FC<CartItemProps> = ({}) => {
+  return <div className="cartitem">cartitem</div>;
+};
