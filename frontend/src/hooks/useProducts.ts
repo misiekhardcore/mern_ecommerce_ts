725afd247a94e17b732dfac36093a6c261cfdd17
misiekhardcore <misiekhardcore@gmail.com>
2021-04-11 21:05:55 +0200

    add redux products fucntions

---
+++ frontend/src/hooks/useProducts.ts
@@ -0,0 +1,39 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getProductDetails,
+  getProducts,
+} from "../redux/actions/productsActions";
+import {
+  IProductsState,
+  IProductState,
+} from "../redux/reducers/productsReducer";
+import { RootState } from "../redux/store";
+
+export const useProducts = (): IProductsState => {
+  const dispatch = useDispatch();
+
+  const products = useSelector<RootState, IProductsState>(
+    (state) => state.getProducts
+  );
+
+  useEffect(() => {
+    dispatch(getProducts());
+  }, [dispatch]);
+
+  return products;
+};
+
+export const useProduct = (id: string): IProductState => {
+  const dispatch = useDispatch();
+
+  const product = useSelector<RootState, IProductState>(
+    (state) => state.getDetails
+  );
+
+  useEffect(() => {
+    dispatch(getProductDetails(id));
+  }, [dispatch, id]);
+
+  return product;
+};
