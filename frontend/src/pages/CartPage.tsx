66f3d30607098211f84e9718c31f537ddbc48e90
misiekhardcore <misiekhardcore@gmail.com>
2021-04-11 21:06:21 +0200

    update CartPage

--- frontend/src/pages/CartPage.tsx
+++ frontend/src/pages/CartPage.tsx
@@ -1,22 +1,32 @@
 import React from "react";
 import { CartItem } from "../components/CartItem";
+import { useCart } from "../hooks/useCart";
+import "./CartPage.scss";
 
 interface CartPageProps {}
 
 export const CartPage: React.FC<CartPageProps> = () => {
+  const { cartItems } = useCart();
+
   return (
-    <div className="cart">
-      <div className="cardpage__left">
-        <h2>Shopping Cart</h2>
-        <CartItem />
-      </div>
-      <div className="cardpage__right">
-        <div className="cartpage__info">
-          <p>Subtotal (0) items</p>
-          <p>$499.99</p>
+    <div className="cartpage">
+      <h2>Shopping Cart</h2>
+      <div className="container">
+        <div className="cartpage__left">
+          {cartItems.map((item) => (
+            <CartItem item={item} />
+          ))}
         </div>
-        <div className="button">
-          <button type="button">Checkout</button>
+        <div className="cartpage__right">
+          <div className="cartpage__info">
+            <p>Subtotal (0) items</p>
+            <p>$499.99</p>
+          </div>
+          <div className="">
+            <button className="button" type="button">
+              Checkout
+            </button>
+          </div>
         </div>
       </div>
     </div>
