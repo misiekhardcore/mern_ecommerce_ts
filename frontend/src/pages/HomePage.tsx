a22c43753d62d5e52fe7b8d51d6b3fedaa34be63
misiekhardcore <misiekhardcore@gmail.com>
2021-04-11 21:07:09 +0200

    use redux store

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,34 +1,26 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Product } from "../components/Product";
-import { IProduct } from "../types";
+import { useProducts } from "../hooks/useProducts";
 import "./HomePage.scss";
 
 interface HomePageProps {}
 
-const initialProducts: IProduct[] = [];
-
 export const HomePage: React.FC<HomePageProps> = () => {
-  const [products, setProducts] = useState<IProduct[]>(initialProducts);
-
-  useEffect(() => {
-    axios
-      .get<IProduct[]>("http://localhost:4000/api/products", {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-      .then((r) => setProducts(r.data))
-      .catch((e) => console.error(e.message));
-  }, []);
+  const { error, loading, products } = useProducts();
 
   return (
     <div className="homepage">
       <h2 className="homepage__title">Latest Products</h2>
       <div className="homepage__products">
-        {products.map((product, idx) => (
-          <Product product={product} key={idx} />
-        ))}
+        {loading ? (
+          <p>Loading...</p>
+        ) : error ? (
+          <p>{error}</p>
+        ) : (
+          products.map((product, idx) => (
+            <Product product={product} key={idx} />
+          ))
+        )}
       </div>
     </div>
   );
--- frontend/src/pages/ProductPage.tsx
+++ frontend/src/pages/ProductPage.tsx
@@ -1,12 +1,14 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router";
+import { ItemCountSelect } from "../components/ItemCountSelect";
+import { useProduct } from "../hooks/useProducts";
+import { addToCart } from "../redux/actions/cartActions";
 import { IProduct } from "../types";
 import "./ProductPage.scss";
 
 interface ProductPageProps {}
 
-const initialState: IProduct = {
+const initialProduct: IProduct = {
   _id: "0",
   price: 0,
   description: "",
@@ -16,65 +18,68 @@ const initialState: IProduct = {
 };
 
 export const ProductPage: React.FC<ProductPageProps> = () => {
+  const [qty, setQty] = useState<number>(0);
   const { id } = useParams<{ id: string }>();
-  const [product, setProduct] = useState<IProduct>(initialState);
+  const { product = initialProduct, loading, error } = useProduct(id);
 
-  useEffect(() => {
-    axios
-      .get<IProduct>("http://172.22.89.171:4000/api/products/" + id, {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-      .then((r) => setProduct(r.data))
-      .catch((e) => console.error(e.message));
-  }, [id]);
-
-  const { name, countInStock, description, imageUrl, price } = product;
-  const options = (count: number) => {
-    let ans: any = [];
-    for (let i = 0; i < count; i++) {
-      ans.push(
-        <option key={i} value={i}>
-          {i}
-        </option>
-      );
-    }
-    return ans;
-  };
+  const {
+    _id,
+    name,
+    countInStock,
+    description,
+    imageUrl,
+    price,
+  } = product as IProduct;
 
   return (
     <div className="productpage">
-      <div className="productpage__left">
-        <div className="left__image">
-          <img src={imageUrl} alt={name} />
-        </div>
-        <div className="left__info">
-          <p className="left__name">{name}</p>
-          <p>{price}</p>
-          <p>{description}</p>
-        </div>
-      </div>
-      <div className="productpage__right">
-        <div className="right__info">
-          <p>
-            Price: <span>${price}</span>
-          </p>
-          <p>
-            Staus:{" "}
-            <span>{countInStock ? "In Stock" : "Not Available"}</span>
-          </p>
-          <p>
-            Qty:
-            <select name="count" id="count">
-              {options(countInStock)}
-            </select>
-          </p>
-          <p>
-            <button type="button">Add to Cart</button>
-          </p>
-        </div>
-      </div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>{error}</p>
+      ) : (
+        <>
+          <div className="productpage__left">
+            <div className="left__image">
+              <img src={imageUrl} alt={name} />
+            </div>
+            <div className="left__info">
+              <p className="left__name">{name}</p>
+              <p>{price}</p>
+              <p>{description}</p>
+            </div>
+          </div>
+          <div className="productpage__right">
+            <div className="right__info">
+              <p>
+                Price: <span>${price}</span>
+              </p>
+              <p>
+                Staus:
+                <span>
+                  {countInStock ? "In Stock" : "Not Available"}
+                </span>
+              </p>
+              <p>
+                Qty:
+                <ItemCountSelect
+                  value={qty}
+                  setValue={setQty}
+                  count={countInStock}
+                />
+              </p>
+              <p>
+                <button
+                  type="button"
+                  onClick={async () => await addToCart(_id, qty)}
+                >
+                  Add to Cart
+                </button>
+              </p>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
