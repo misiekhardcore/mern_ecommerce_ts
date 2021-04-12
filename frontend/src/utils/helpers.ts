b010a84b11cfc61e020558b2e312a73d1d839548
misiekhardcore <misiekhardcore@gmail.com>
2021-04-12 13:56:41 +0200

    add totalAmount and totalPrice functions

---
+++ frontend/src/utils/helpers.ts
@@ -0,0 +1,7 @@
+import { ICartItem } from "../redux/reducers/cartReducer";
+
+export const totalAmount = (cart: ICartItem[]): number =>
+  cart.reduce<number>((a, c) => a + c.amount, 0);
+
+export const totalPrice = (cart: ICartItem[]): number =>
+  cart.reduce<number>((a, c) => a + c.amount * c.product.price, 0);
