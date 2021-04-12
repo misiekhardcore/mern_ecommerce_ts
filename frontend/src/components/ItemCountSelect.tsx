55c2e6291f336c6876a815e6da4b657e8227bc8b
misiekhardcore <misiekhardcore@gmail.com>
2021-04-12 13:51:04 +0200

    change typing,defaults and disable function

--- frontend/src/components/ItemCountSelect.tsx
+++ frontend/src/components/ItemCountSelect.tsx
@@ -1,31 +1,34 @@
 interface ItemCountSelectProps {
   count: number;
   value: number;
-  setValue: React.Dispatch<React.SetStateAction<number>>;
+  setValue: any;
   current?: number;
   className?: string;
 }
 
 export const ItemCountSelect: React.FC<ItemCountSelectProps> = ({
-  count,
-  value,
+  count = 0,
+  value = 0,
   setValue,
-  current,
   className,
 }) => {
   const options = (count: number) => {
     let ans: any = [];
-    for (let i = 0; i < count; i++) {
-      ans.push(
-        <option key={i} value={i}>
-          {i}
-        </option>
-      );
+    if (count) {
+      for (let i = 1; i <= count; i++) {
+        ans.push(
+          <option key={i} value={i}>
+            {i}
+          </option>
+        );
+      }
     }
+
     return ans;
   };
   return (
     <select
+      disabled={!count}
       onChange={(e) => {
         setValue(parseInt(e.target.value));
       }}
