9eae5684a9cb4267b5333672da1914e3eb036094
misiekhardcore <misiekhardcore@gmail.com>
2021-08-18 18:57:22 +0200

    fixes

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,11 +1,11 @@
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 import mongoose from "mongoose";
 
-dotenv.config();
+// dotenv.config();
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(process.env.MONGO_URI || "", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
